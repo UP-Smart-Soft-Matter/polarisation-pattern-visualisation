dad78c99779105440b5cc73394bdc1213b6ce28f
anghofus <github@angho.de>
2026-03-31 16:31:01 +0200

    .

--- visualizer.py
+++ visualizer.py
@@ -1,22 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from PIL.GimpGradientFile import linear
 
 x_max = 17
-alpha = -5
+alpha = -2
 y_min = 0
 y_max = 255
-arrow_number = 15
+arrow_number_per_period = 6
 linear_flag = True
-A = 1.5
-A_px = 20
+A = 30
+length = 36
+m_y = -25
 
 def exponential_sawtooth(phase, alpha):
     return (np.exp(alpha * phase) - 1) / (np.exp(alpha) - 1)
 
-def get_arrow_start_end(m_x, m_y, A, gs, a_y_scale):
-    phi = np.radians(gs * (180/255))
-    a_vector = A/2 * np.array([np.cos(phi), (np.sin(phi)*a_y_scale)])
+def get_arrow_start_end(m_x, m_y, A, gs, px_per_x_unit, px_per_y_unit):
+    phi = np.radians(gs * (180/255)) + np.pi/2
+    a_vector = A/2 * np.array([np.cos(phi) * (1/px_per_x_unit), np.sin(phi) * (1/px_per_y_unit)])
     m_vector = np.array([m_x, m_y])
     x = m_vector - a_vector
     y = m_vector + a_vector
@@ -24,11 +24,12 @@ def get_arrow_start_end(m_x, m_y, A, gs, a_y_scale):
     return (x[0], x[1]), (y[0], y[1])
 
 if linear_flag:
-    sawtooth = lambda: x
+    sawtooth = lambda: phase
 else:
-    sawtooth = lambda: exponential_sawtooth(x, alpha)
+    sawtooth = lambda: exponential_sawtooth(phase, alpha)
 
-x = np.linspace(0, 1, x_max)
+x = np.arange(length)
+phase = (x % x_max) / (x_max - 1)
 values = y_min + sawtooth() * (y_max - y_min)
 
 fig, ax = plt.subplots(figsize=(6, 4), dpi=100)  # 6x4 inch, 100 dpi → 600x400 Pixel
@@ -39,31 +40,28 @@ ax.set_xlabel('Width [px]')
 ax.set_ylabel('gray value')
 ax.set_ylim(-50, 260)
 
-# Transformationsmatrix von Daten -> Figure
-bbox = ax.get_window_extent().transformed(fig.dpi_scale_trans.inverted())
-width_inch, height_inch = bbox.width, bbox.height
+fig.canvas.draw()
+trans = ax.transData
 
-# Datenbereich
-x_range = ax.get_xlim()[1] - ax.get_xlim()[0]
-y_range = ax.get_ylim()[1] - ax.get_ylim()[0]
+origin = trans.transform((0, 0))
+x_unit = trans.transform((1, 0))
+y_unit = trans.transform((0, 1))
 
-# Pixel pro Daten-Einheit
-dpi = fig.dpi
-px_per_data_x = (width_inch * dpi) / x_range
-px_per_data_y = (height_inch * dpi) / y_range
+px_per_x_unit = x_unit[0] - origin[0]
+px_per_y_unit = y_unit[1] - origin[1]
 
-a_y_scale = A_px / px_per_data_y
 
-m_y = 0
+arrow_number = int((length / x_max)*arrow_number_per_period)
+
 value_index = np.linspace(0, len(values) -1, arrow_number)
-for i, m_x in enumerate(np.linspace(0, x_max, arrow_number)):
+for i, m_x in enumerate(np.linspace(0, length-1, arrow_number)):
     gs = values[int(value_index[i])]
-    x, y = get_arrow_start_end(m_x, m_y, A, gs, a_y_scale)
+    x, y = get_arrow_start_end(m_x, m_y, A, gs, px_per_x_unit, px_per_y_unit)
     ax.annotate(
         '',
         xy=y,  # Ende
         xytext=x,  # Start
-        arrowprops=dict(arrowstyle='->', color='red')
+        arrowprops=dict(arrowstyle='<->', color='red')
     )
 fig.tight_layout()
 plt.show()
